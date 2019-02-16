e4e7aa3d69137315bd6df73ee702f7263b51480f
Martin Nestorov <mdinestorov@gmail.com>
2019-02-17 00:06:04 +0200

    Adds automatic map evaluation of the route

--- templates/map.js
+++ templates/map.js
@@ -1,5 +1,17 @@
 const meanRadius = 6378137; // Earth’s mean radius in meter
 
+let toLat = 42.264444;
+let toLong = 23.606944;
+let fromLat = 42.179722;
+let fromLong = 23.586667;
+let mapTo;
+let mapFrom;
+let routeComplexity = "green";
+
+// Google API defined
+let directionsService;
+let directionsDisplay;
+
 let rad = function(x) {
     return x * Math.PI / 180;
 };
@@ -19,4 +31,37 @@ let getDistance = function(p1Lat, p1Long, p2Lat, p2Long) {
     return distanceMeters / 1000;
 };
 
-getDistance(41.7677, 23.4008, 42.1797, 23.5866);
+function initMap() {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer({
+	polylineOptions: {
+	    strokeColor: routeComplexity
+	}
+    });
+
+    mapTo = new google.maps.LatLng(toLat, toLong);
+    mapFrom = new google.maps.LatLng(fromLat, fromLong);
+    let mapOptions = {
+	zoom: 14,
+	center: mapFrom
+    };
+
+    let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+    calcRoute();
+}
+
+function calcRoute() {
+    let request = {
+	origin: mapFrom,
+	destination: mapTo,
+	travelMode: google.maps.TravelMode['WALKING']
+    };
+    directionsService.route(request, function(response, status) {
+	if (status === 'OK') {
+	    directionsDisplay.setDirections(response);
+	}
+    });
+}
+
+calcRoute();
