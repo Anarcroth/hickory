771dd1e3ea4f05f41f731474b71a58ca7e39370f
Martin Nestorov <mdinestorov@gmail.com>
2019-02-16 15:19:05 +0200

    Adds connection to the DB

--- flask_run.sh
+++ flask_run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+export POSTGRES_URL="172.16.191.230:5432"
+export POSTGRES_PORT="5432"
+export POSTGRES_USER="postgres"
+export POSTGRES_PW="dbpw"
+export POSTGRES_DB="test"
 FLASK_APP=hickory.py flask run
--- hickory.py
+++ hickory.py
@@ -1,3 +1,9 @@
+#!/bin/env python
+
+import psycopg2
+import re
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,3 +15,13 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    get_connection()
+
+
+def get_connection():
+    conn = psycopg2.connect(databasename=POSTGRES_DB,
+                           user=POSTGRES_USER,
+                           password=POSTGRES_PW,
+                           host=POSTGRES_URL,
+                           port=POSTGRES_PORT)
+    return conn
