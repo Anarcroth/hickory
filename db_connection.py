9cd262f02bea85f79391fa60d7178959afbe56c5
Dzhovani <dzhovani.chemishanov@strypes.eu>
2019-02-16 11:55:37 +0200

    prepare db connection

---
+++ db_connection.py
@@ -0,0 +1,23 @@
+"""module to establish db connection."""
+from flask_sqlalchemy import SQLAlchemy
+
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+POSTGRES_URL = get_env_variable("POSTGRES_URL")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PW = get_env_variable("POSTGRES_PW")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+
+db = SQLAlchemy(app)
--- flask_run.sh
+++ flask_run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+export POSTGRES_URL="127.0.0.1:5432"
+export POSTGRES_USER="postgres"
+export POSTGRES_PW="dbpw"
+export POSTGRES_DB="test"
 FLASK_APP=hickory.py flask run
