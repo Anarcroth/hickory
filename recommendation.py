4be1e80bac51169951ed555459257357db11b693
Martin Nestorov <mdinestorov@gmail.com>
2019-02-16 18:19:44 +0200

    Adds recommendation evaluation

--- recommendation.py
+++ recommendation.py
@@ -1,5 +1,7 @@
 import math
 
+max_coefficients = [1000000, 2500, 10000, 100]
+
 
 def get_users():
     pass
@@ -10,12 +12,17 @@ def get_list_routes():
 
 
 def normalize(vector):
-    length = math.sqrt(sum([math.pow(i, 2) for i in vector]))
-    for item in vector:
-        item = float(item) / length
+    normalize_values = [u / m for u, m in zip(vector, max_coefficients)]
+    length = math.sqrt(sum([math.pow(i, 2) for i in normalize_values]))
+    for i, item in enumerate(normalize_values):
+        vector[i] = float(item) / length
+    return vector
+
+
+def weight(vector):
     coefficients = [0.2, 0.2, 0.2, 0.4]
-    for index in range(len(vector)):
-        vector[index] *= coefficients[index]
+    for i, item in enumerate(vector):
+        vector[i] = coefficients[i] * item
     return vector
 
 
@@ -25,25 +32,28 @@ def check_prev_routes():
 
 def search_nearest_route():
     user = [1000, 250, 10, 10]  # get_users()
-    routes = [[100, 250, 8, 10], [1000, 100, 3, 2], [100000, 400, 9, 2]]
+    routes = [[1000, 100, 3, 2], [100000, 400, 9, 2], [100, 250, 8, 10]]
     #get_list_routes()
 
     user = normalize(user)
-    for route in routes:
-        route = normalize(route)
+    for i, route in enumerate(routes):
+        routes[i] = normalize(route)
+        routes[i] = weight(route)
 
     nearest_routes = sort_by_nearest(routes, user)
     print(nearest_routes)
 
 
-def sort_by_nearest(routes, comparator):
+def sort_by_nearest(routes, user):
     closest_dist = []
     for route in routes:
         temp_dist = 0
-        for index in range(len(route)):
-            temp_dist += abs(comparator[index] - route[index])
-            closest_dist.append(temp_dist)
-    return closest_dist.sort()
+        for i, index in enumerate(route):
+            temp_dist += abs(user[i] - route[i])
+        closest_dist.append(temp_dist)
+        print(temp_dist)
+
+    return sorted(closest_dist)
 
 
 search_nearest_route()
