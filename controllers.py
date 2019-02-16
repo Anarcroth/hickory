f51dbcf85e2876c06c64d0e0ed75badc0da3e6f5
dchemishanov <dzhovani.chemishanov@strypes.eu>
2019-02-16 20:15:09 +0200

    rework recommendation system to work and return routes objects

--- controllers.py
+++ controllers.py
@@ -3,7 +3,7 @@ import re
 import os
 import math
 
-from constants import MAX_COEFFICIENTS
+from constants import COEFFICIENTS, MAX_COEFFICIENTS
 
 
 
@@ -36,34 +36,30 @@ class Recommendation(object):
 
     @staticmethod
     def weight(vector : list) -> list:
-        coefficients = [0.2, 0.2, 0.2, 0.4]
         for i, item in enumerate(vector):
-            vector[i] = coefficients[i] * item
+            vector[i] = COEFFICIENTS[i] * item
         return vector
 
     @staticmethod
     def sort_by_nearest(user : list, routes : list) -> list:
-        closest_dist = []
         for route in routes:
-            temp_dist = 0
-            for i, index in enumerate(route):
-                temp_dist += abs(user[i] - route[i])
-            closest_dist.append(temp_dist)
+            for i, index in enumerate(route.profile_vector):
+                route.approximation += abs(user[i] - route.profile_vector[i])
             #print(temp_dist)
-        return sorted(closest_dist)
+        return sorted(routes, key=lambda r: r.approximation)
 
     @staticmethod
     def search_nearest_route(user : list = None, routes : list = None) -> list:
         if not user:
             user = [1000, 250, 10, 10]  # get_users()
         if not routes:
-            routes = [[1000, 100, 3, 2], [100000, 400, 9, 2], [100, 250, 8, 10]]
+            routes = [Route(profile_vector=v) for v in [[1000, 100, 3, 2], [100000, 400, 9, 2], [100, 250, 8, 10]]]
         #get_list_routes()
 
         user = Recommendation.normalize(user)
         for i, route in enumerate(routes):
-            routes[i] = Recommendation.normalize(route)
-            routes[i] = Recommendation.weight(route)
+            routes[i].profile_vector = Recommendation.normalize(route.profile_vector)
+            routes[i].profile_vector = Recommendation.weight(route.profile_vector)
 
         nearest_routes = Recommendation.sort_by_nearest(user, routes)
         return nearest_routes
@@ -78,12 +74,23 @@ class User(object):
 
 
 class Route(object):
+    def __init__(self, id : int = None, profile_vector : list = None):
+        self.id = id
+        self.approximation = 0.0
+        if not profile_vector:
+            self.profile_vector = [1, 2, 3, 4]
+        else:
+            self.profile_vector = profile_vector
+
     def get(self, id : int):
         pass
 
     @staticmethod
     def get_all():
         return []
+
+    def __repr__(self):
+        return str({self.approximation: self.profile_vector})
 
 
 class UserSetting(object):
